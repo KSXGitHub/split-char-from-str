f09d96de17273de29da1e759231c2006108c6017
khai96_ <hvksmr1996@gmail.com>
2024-10-16 12:20:42 +0700

    fix: let method inherit lifetime

--- lib.rs
+++ lib.rs
@@ -12,23 +12,38 @@ pub fn split_first_char(text: &str) -> Option<(char, &'_ str)> {
 }
 
 /// Convenient trait to call [`split_first_char`] as a method.
-pub trait SplitFirstChar: Deref<Target = str> {
+pub trait SplitFirstChar<'a>: Deref<Target = str> + Sized {
     /// Split a string into a pair of first character and the rest.
-    fn split_first_char(&self) -> Option<(char, &'_ str)> {
+    fn split_first_char(self) -> Option<(char, &'a str)>;
+}
+
+impl<'a> SplitFirstChar<'a> for &'a str {
+    fn split_first_char(self) -> Option<(char, &'a str)> {
         split_first_char(self)
     }
 }
 
-impl<Str: Deref<Target = str>> SplitFirstChar for Str {}
-
 #[cfg(test)]
 mod test {
-    use super::SplitFirstChar;
+    use super::{split_first_char, SplitFirstChar};
 
     #[test]
     fn test_split_first_char() {
         assert_eq!("abc".split_first_char(), Some(('a', "bc")));
         assert_eq!("x".split_first_char(), Some(('x', "")));
         assert_eq!("".split_first_char(), None);
+    }
+
+    #[test]
+    fn lifetime() {
+        fn _use_function(text: &str) -> Option<&'_ str> {
+            let (_, rest) = split_first_char(text)?;
+            Some(rest)
+        }
+
+        fn _use_method(text: &str) -> Option<&'_ str> {
+            let (_, rest) = text.split_first_char()?;
+            Some(rest)
+        }
     }
 }
